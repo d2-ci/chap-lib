6229c8f6b7c8c5bdb702ed92572ab8e6b322ff6f
GitHub <noreply@github.com>
2025-04-25 10:44:32 +0000

    b5b77b9 feat: fix release note less than 100 char

---
+++ build/es/httpfunctions/models/PredictionInfo.d.ts
@@ -0,0 +1,9 @@
+export type PredictionInfo = {
+    datasetId: number;
+    modelId: string;
+    nPeriods: number;
+    name: string;
+    created: string;
+    metaData?: Record<string, any>;
+    id: number;
+};
