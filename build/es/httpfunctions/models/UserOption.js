c7e48823301bbc540fc425521914592b7f4580e3
Birk Johansson <birk@dhis2.org>
2025-05-14 12:28:58 +0000

    9f1b52a fix: refactor layout to accomodate sidebar

---
+++ build/es/httpfunctions/models/UserOption.js
@@ -0,0 +1,15 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export let UserOption;
+(function (_UserOption) {
+  let type = /*#__PURE__*/function (type) {
+    type["STRING"] = "string";
+    type["INTEGER"] = "integer";
+    type["FLOAT"] = "float";
+    type["BOOLEAN"] = "boolean";
+    return type;
+  }({});
+  _UserOption.type = type;
+})(UserOption || (UserOption = {}));
