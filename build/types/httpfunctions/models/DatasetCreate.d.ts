2f2a84ad1ddebbe03f425f7f90c2c0d306e69447
Herman Tretteteig <hermat@uio.no>
2025-05-16 09:23:32 +0000

    51f710e fix: eval overflow view

---
+++ build/types/httpfunctions/models/DatasetCreate.d.ts
@@ -0,0 +1,9 @@
+import type { FeatureCollectionModel } from './FeatureCollectionModel';
+import type { ObservationBase } from './ObservationBase';
+export type DatasetCreate = {
+    name: string;
+    type?: (string | null);
+    geojson: FeatureCollectionModel;
+    observations: Array<ObservationBase>;
+};
+//# sourceMappingURL=DatasetCreate.d.ts.map
