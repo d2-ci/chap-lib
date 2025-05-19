33008847dbdc3b58e8cc69fb5050b78984a704d3
Birk Johansson <birk@dhis2.org>
2025-05-19 14:54:30 +0000

    ec49aa1 fix: add empty selection introduction

---
+++ build/types/httpfunctions/models/FetchRequest.d.ts
@@ -0,0 +1,5 @@
+export type FetchRequest = {
+    featureName: string;
+    dataSourceName: string;
+};
+//# sourceMappingURL=FetchRequest.d.ts.map
---
+++ src/components/evaluation/SplitPeriodSelector/SplitPeriodSelector.tsx
@@ -0,0 +1,48 @@
+import {
+    Checkbox,
+    SingleSelect,
+    SingleSelectField,
+    SingleSelectFieldProps,
+    SingleSelectOption,
+} from '@dhis2/ui'
+import React from 'react'
+import { getPeriodNameFromId } from '../../../utils/Time'
+
+interface SplitPeriodSelectorProps extends SingleSelectFieldProps {
+    splitPeriods: string[]
+    setSelectedSplitPeriod: (splitPeriod: string) => void
+    selectedSplitPeriod: string
+}
+
+const SplitPeriodSelector = ({
+    splitPeriods,
+    setSelectedSplitPeriod,
+    selectedSplitPeriod,
+    ...singleSelectFieldProps
+}: SplitPeriodSelectorProps) => {
+    if (!splitPeriods.includes(selectedSplitPeriod)) {
+        return <></>
+    }
+
+    return (
+        <div>
+            <div>
+                <SingleSelectField
+                    {...singleSelectFieldProps}
+                    selected={selectedSplitPeriod}
+                    onChange={(e) => setSelectedSplitPeriod(e.selected)}
+                >
+                    {splitPeriods.map((splitPeriod, i) => (
+                        <SingleSelectOption
+                            key={i}
+                            label={getPeriodNameFromId(splitPeriod)}
+                            value={splitPeriod}
+                        />
+                    ))}
+                </SingleSelectField>
+            </div>
+        </div>
+    )
+}
+
+export default SplitPeriodSelector
