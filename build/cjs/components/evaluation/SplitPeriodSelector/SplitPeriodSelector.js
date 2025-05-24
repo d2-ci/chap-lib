2c2703e42055d1622639d97d450350b147a37673
eirikhaugstulen <eirik.haugstulen@gmail.com>
2025-05-24 10:02:44 +0000

    61008b9 feat: add CancelJobModal component and integrate it into JobActionsMenu

---
+++ build/cjs/components/evaluation/SplitPeriodSelector/SplitPeriodSelector.js
@@ -0,0 +1,28 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _ui = require("@dhis2/ui");
+var _react = _interopRequireDefault(require("react"));
+var _Time = require("../../../utils/Time");
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
+const SplitPeriodSelector = ({
+  splitPeriods,
+  setSelectedSplitPeriod,
+  selectedSplitPeriod
+}) => {
+  if (!splitPeriods.includes(selectedSplitPeriod)) {
+    return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null);
+  }
+  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement(_ui.SingleSelectField, {
+    selected: selectedSplitPeriod,
+    onChange: e => setSelectedSplitPeriod(e.selected)
+  }, splitPeriods.map((splitPeriod, i) => /*#__PURE__*/_react.default.createElement(_ui.SingleSelectOption, {
+    key: i,
+    label: (0, _Time.getPeriodNameFromId)(splitPeriod),
+    value: splitPeriod
+  })))));
+};
+var _default = exports.default = SplitPeriodSelector;
---
+++ build/types/httpfunctions/services/JobsService.d.ts
@@ -0,0 +1,65 @@
+import type { DataBaseResponse } from '../models/DataBaseResponse';
+import type { EvaluationResponse } from '../models/EvaluationResponse';
+import type { FullPredictionResponse } from '../models/FullPredictionResponse';
+import type { JobDescription } from '../models/JobDescription';
+import type { CancelablePromise } from '../core/CancelablePromise';
+export declare class JobsService {
+    /**
+     * List Jobs
+     * List all jobs currently in the queue
+     * @returns JobDescription Successful Response
+     * @throws ApiError
+     */
+    static listJobsJobsGet(): CancelablePromise<Array<JobDescription>>;
+    /**
+     * Get Job Status
+     * @param jobId
+     * @returns string Successful Response
+     * @throws ApiError
+     */
+    static getJobStatusJobsJobIdGet(jobId: string): CancelablePromise<string>;
+    /**
+     * Delete Job
+     * @param jobId
+     * @returns any Successful Response
+     * @throws ApiError
+     */
+    static deleteJobJobsJobIdDelete(jobId: string): CancelablePromise<Record<string, any>>;
+    /**
+     * Cancel Job
+     * Cancel a running job
+     * @param jobId
+     * @returns any Successful Response
+     * @throws ApiError
+     */
+    static cancelJobJobsJobIdCancelPost(jobId: string): CancelablePromise<Record<string, any>>;
+    /**
+     * Get Logs
+     * @param jobId
+     * @returns string Successful Response
+     * @throws ApiError
+     */
+    static getLogsJobsJobIdLogsGet(jobId: string): CancelablePromise<string>;
+    /**
+     * Get Prediction Result
+     * @param jobId
+     * @returns FullPredictionResponse Successful Response
+     * @throws ApiError
+     */
+    static getPredictionResultJobsJobIdPredictionResultGet(jobId: string): CancelablePromise<FullPredictionResponse>;
+    /**
+     * Get Evaluation Result
+     * @param jobId
+     * @returns EvaluationResponse Successful Response
+     * @throws ApiError
+     */
+    static getEvaluationResultJobsJobIdEvaluationResultGet(jobId: string): CancelablePromise<EvaluationResponse>;
+    /**
+     * Get Database Result
+     * @param jobId
+     * @returns DataBaseResponse Successful Response
+     * @throws ApiError
+     */
+    static getDatabaseResultJobsJobIdDatabaseResultGet(jobId: string): CancelablePromise<DataBaseResponse>;
+}
+//# sourceMappingURL=JobsService.d.ts.map
