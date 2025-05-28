9b2b9d371c095fb9fe38d27db21703960853cb74
GitHub <noreply@github.com>
2025-05-28 19:34:29 +0000

    1c9e225 feat: add functionality for revoking jobs (#49)

---
+++ build/types/components/evaluation/ComparisonPlotList/ComparisonPlotList.d.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+import { EvaluationPerOrgUnit } from '../../../interfaces/Evaluation';
+interface ComparisonPlotListProps {
+    evaluationPerOrgUnits: EvaluationPerOrgUnit[];
+    useVirtuoso?: boolean;
+    useVirtuosoWindowScroll?: boolean;
+}
+export declare const ComparisonPlotList: React.FC<ComparisonPlotListProps>;
+export {};
+//# sourceMappingURL=ComparisonPlotList.d.ts.map
---
+++ build/types/httpfunctions/models/PredictionResponse.d.ts
@@ -0,0 +1,7 @@
+export type PredictionResponse = {
+    orgUnit: string;
+    dataElement: string;
+    period: string;
+    value: number;
+};
+//# sourceMappingURL=PredictionResponse.d.ts.map
