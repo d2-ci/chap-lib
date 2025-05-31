8af854ce46853f8484c8a1548a3617e107d0c640
eirikhaugstulen <eirik.haugstulen@gmail.com>
2025-05-31 09:16:22 +0000

    916aa64 feat!: release v3

---
+++ build/types/httpfunctions/models/FeatureCollectionModel.d.ts
@@ -0,0 +1,6 @@
+import type { FeatureModel } from './FeatureModel';
+export type FeatureCollectionModel = {
+    type?: string;
+    features: Array<FeatureModel>;
+};
+//# sourceMappingURL=FeatureCollectionModel.d.ts.map
