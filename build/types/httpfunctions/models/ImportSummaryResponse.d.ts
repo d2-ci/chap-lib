1d97aca43afdeeda09707af7dddcd69de75241ca
GitHub <noreply@github.com>
2025-05-31 00:26:46 +0000

    04bae8e feat: starting point for new evaluation page (#45)

---
+++ build/types/httpfunctions/models/ImportSummaryResponse.d.ts
@@ -0,0 +1,7 @@
+import type { ValidationError } from './ValidationError';
+export type ImportSummaryResponse = {
+    id: string;
+    imported_count: number;
+    rejected: Array<ValidationError>;
+};
+//# sourceMappingURL=ImportSummaryResponse.d.ts.map
