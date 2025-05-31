70840cff328feb0edcfdacd4dd65d889fecad0d0
eirikhaugstulen <eirik.haugstulen@gmail.com>
2025-05-31 08:51:46 +0000

    d51460a chore: update chap minimum version

---
+++ build/types/httpfunctions/models/DatasetMakeRequest.d.ts
@@ -0,0 +1,11 @@
+import type { FeatureCollectionModel } from './FeatureCollectionModel';
+import type { FetchRequest } from './FetchRequest';
+import type { ObservationBase } from './ObservationBase';
+export type DatasetMakeRequest = {
+    name: string;
+    type?: (string | null);
+    geojson: FeatureCollectionModel;
+    providedData: Array<ObservationBase>;
+    dataToBeFetched: Array<FetchRequest>;
+};
+//# sourceMappingURL=DatasetMakeRequest.d.ts.map
