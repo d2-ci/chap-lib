9bea92283921c27787391ae4acf657693568a61a
Birk Johansson <birk@dhis2.org>
2025-06-01 20:07:50 +0000

    ec156f9 fix(orgunits): sorted units and clear all button

---
+++ build/types/Globals.d.d.ts
@@ -0,0 +1,2 @@
+declare module '@dhis2/maps-gl';
+//# sourceMappingURL=Globals.d.d.ts.map
---
+++ build/types/components/predictions/UncertaintyAreaChart/UncertaintyAreaChart.d.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+import { FullPredictionResponseExtended } from '../../../interfaces/Prediction';
+interface PredicationChartProps {
+    data: FullPredictionResponseExtended;
+    predictionTargetName: string;
+}
+export declare const UncertaintyAreaChart: ({ data, predictionTargetName, }: PredicationChartProps) => React.JSX.Element;
+export {};
+//# sourceMappingURL=UncertaintyAreaChart.d.ts.map
