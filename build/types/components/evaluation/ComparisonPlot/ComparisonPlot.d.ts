d0b77faefe3c305936e6c31160d86b11cddad1b0
eirikhaugstulen <eirik.haugstulen@gmail.com>
2025-06-01 22:50:45 +0000

    c1ec87f fix: dependency resolution

---
+++ build/types/components/evaluation/ComparisonPlot/ComparisonPlot.d.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+import { EvaluationPerOrgUnit } from '../../../interfaces/Evaluation';
+interface SideBySidePlotsProps {
+    orgUnitsData: EvaluationPerOrgUnit;
+}
+export declare const ComparisonPlot: React.FC<SideBySidePlotsProps>;
+export {};
+//# sourceMappingURL=ComparisonPlot.d.ts.map
