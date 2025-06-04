04d3b7774c3a3d6af4f6089ebf5b0e8dc938f60a
eirikhaugstulen <eirik.haugstulen@gmail.com>
2025-06-04 14:18:07 +0000

    121fa33 fix(ModelTemplateConfigForm): add Enter key functionality for adding new covariates

---
+++ build/types/httpfunctions/models/AuthorAssessedStatus.d.ts
@@ -0,0 +1,8 @@
+export declare enum AuthorAssessedStatus {
+    GRAY = "gray",
+    RED = "red",
+    ORANGE = "orange",
+    YELLOW = "yellow",
+    GREEN = "green"
+}
+//# sourceMappingURL=AuthorAssessedStatus.d.ts.map
---
+++ build/types/httpfunctions/models/MakePredictionRequest.d.ts
@@ -0,0 +1,13 @@
+import type { FeatureCollectionModel } from './FeatureCollectionModel';
+import type { FetchRequest } from './FetchRequest';
+import type { ObservationBase } from './ObservationBase';
+export type MakePredictionRequest = {
+    name: string;
+    type?: (string | null);
+    geojson: FeatureCollectionModel;
+    providedData: Array<ObservationBase>;
+    dataToBeFetched: Array<FetchRequest>;
+    modelId: string;
+    metaData?: Record<string, any>;
+};
+//# sourceMappingURL=MakePredictionRequest.d.ts.map
