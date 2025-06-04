7781b6f8479e1138a54b6b8b3715abfa237e70d8
eirikhaugstulen <eirik.haugstulen@gmail.com>
2025-06-04 23:30:43 +0000

    ca8e849 feat: add copy functionality for backtests with a new modal

---
+++ build/types/components/evaluation/ComparisonPlotList/ComparisonPlotList.d.ts
@@ -0,0 +1,13 @@
+import React from 'react';
+import { EvaluationPerOrgUnit } from '../../../interfaces/Evaluation';
+import { VirtuosoProps } from 'react-virtuoso';
+interface ComparisonPlotListProps {
+    evaluationPerOrgUnits: EvaluationPerOrgUnit[];
+    useVirtuoso?: boolean;
+    useVirtuosoWindowScroll?: boolean;
+    virtuosoProps?: VirtuosoProps<any, any>;
+    nameLabel?: string;
+}
+export declare const ComparisonPlotList: React.FC<ComparisonPlotListProps>;
+export {};
+//# sourceMappingURL=ComparisonPlotList.d.ts.map
---
+++ build/types/components/evaluation/ResultPlot/ResultPlot.d.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+import { HighChartsData } from '../../../interfaces/Evaluation';
+interface ResultPlotProps {
+    data: HighChartsData;
+    modelName: string;
+    nameLabel?: string;
+    syncZoom: boolean;
+}
+export declare const ResultPlot: ({ data, modelName, syncZoom, nameLabel, }: ResultPlotProps) => React.JSX.Element;
+export {};
+//# sourceMappingURL=ResultPlot.d.ts.map
