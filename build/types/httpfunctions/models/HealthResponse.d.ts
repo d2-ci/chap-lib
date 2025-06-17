43b64c390f5c131eedb246168578f82a3a78f250
Birk Johansson <birk@dhis2.org>
2025-06-17 15:31:36 +0000

    79b04ba refactor: minor cleanup

---
+++ build/types/httpfunctions/models/HealthResponse.d.ts
@@ -0,0 +1,5 @@
+export type HealthResponse = {
+    status: string;
+    message: string;
+};
+//# sourceMappingURL=HealthResponse.d.ts.map
---
+++ build/types/httpfunctions/models/ObservationBase.d.ts
@@ -0,0 +1,7 @@
+export type ObservationBase = {
+    period: string;
+    orgUnit: string;
+    value: (number | null);
+    featureName: (string | null);
+};
+//# sourceMappingURL=ObservationBase.d.ts.map
